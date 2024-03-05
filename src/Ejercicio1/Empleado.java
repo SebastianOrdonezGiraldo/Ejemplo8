54148aa2fcd4b045d66bbae1602bd321403576df
Sebastian Ordoñez Giraldo <sebastian789go@gmail.com>
2024-03-05 16:17:45 -0500

    ejercicio 2 completo

--- src/Ejercicio1/Empleado.java
+++ src/Ejercicio1/Empleado.java
@@ -1,6 +1,9 @@
 package Ejercicio1;
 
 
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.ZoneId;
 import java.util.Date;
 
 
@@ -19,6 +22,11 @@ public class Empleado {
         this.fechaNacimiento = fechaNacimiento;
         this.fechaIngreso = fechaIngreso;
         this.salarioBasico = salarioBasico;
+    }
+    public int calcularEdad() {
+        LocalDate ahora = LocalDate.now();
+        LocalDate fechaNac = fechaNacimiento.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        return Period.between(fechaNac, ahora).getYears();
     }
 
     public String getNombre() {
